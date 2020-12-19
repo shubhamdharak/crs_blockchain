01a63768bb150a86ae4e4796b3f036eab75688af
msaurabh50702 <msaurabh50702@gmail.com>
2020-12-19 13:59:50 +0530

    Goverment portal layout designed, Solve auto session expiry problem

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,12 +1,14 @@
 module.exports ={
     dashboard: (req, res)=> {
-        if(req.session.isValidUser)
+        console.log(req.session.isValidUser)
+        if(req.session.isValidUser === true){
             if(req.session.userRole === "goverment"){
                 res.render("govDashboard",{isValid:true,userRole:req.session.userRole})
             }
             else if(req.session.userRole === "vendor"){
                 res.render("userDashboard",{isValid:true,userRole:req.session.userRole})
             }
+        }
         else{
             req.flash('error', "Session Expired")
             res.redirect("logout")
