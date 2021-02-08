ecd9fcd47dc690a5f8e2d9343f016d5428747402
shubhamdharak <shubhamdharak1133@gmail.com>
2021-02-08 13:06:21 +0530

    Added error message clearly

--- initDB.js
+++ initDB.js
@@ -5,9 +5,9 @@ mongoose.connect(process.env.MONGODB_URL  || 'mongodb://localhost:27017/crs_db',
 
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, `connection error:`));
 db.once('open', function() {
-  console.log('mongodb connected!');
+  console.log('Mongodb connected!');
 });
 
 module.exports = db
