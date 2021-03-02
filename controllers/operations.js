b2635dc1cb5ad722dab1657e19001fff83429466
shubhamdharak <shubhamdharak1133@gmail.com>
2021-03-02 22:00:32 +0530

    [fix] : Delete scheme

--- controllers/operations.js
+++ controllers/operations.js
@@ -7,16 +7,24 @@ module.exports = {
             console.log(e.message);
             req.flash('error', "Cannot connect to network")
         })
-        const allSchemes =  connection.initContract().methods.allSchemes().call();
-        allSchemes
-            .then((r) => {
-                res.render('index', {allSchemes: r})
-            })
-            .catch((error) => {
-                req.flash('error',`  something went wrong, please try later`)
-                console.log(error.message);
-                return res.render('index')
-            });
+        try {
+            const allSchemes =  await connection.initContract().methods.allSchemes().call();
+            return res.render('index', {allSchemes: allSchemes})
+        } catch(error) {
+            console.log(error.message);
+            req.flash('error', "Something went wrong!!")
+            return res.render('index')
+        }
+
+        // allSchemes
+        //     .then((r) => {
+        //         return res.render('index', {allSchemes: r})
+        //     })
+        //     .catch((error) => {
+        //         req.flash('error',`  something went wrong, please try later`)
+        //         console.log(error.message);
+        //         return res.render('index')
+        //     });
     },
     getAScheme: async (req, res) => {
         connection.initWeb3().catch(e=> {
@@ -35,7 +43,7 @@ module.exports = {
     addScheme: async (req, res) => {
         const { name, cost, description, date } = req.body
         try {
-            const scheme = await connection.initContract().methods.addScheme(name, date, description,cost).send({from: '0x320f78b8930dAe81880f2F63E7Cf1371a7700aFd', gas:3000000})
+            const scheme = await connection.initContract().methods.addScheme(name, date, description,cost).send({from: '0xc83DF5D66DBb599072D80bCd9712c6D717eC8528', gas:3000000})
             console.log(scheme);
             const allSchemes = await connection.initContract().methods.allSchemes().call()
             return  res.render("govDashboard",{isValid:true,userRole:req.session.userRole,allSchemes:allSchemes })
@@ -47,11 +55,10 @@ module.exports = {
         }
     },
     deleteScheme: async (req, res)=> {
-        const id  = parseInt(req.params.id)
+        const scheme_id  = parseInt(req.params.id)
         try {
-            const result = await connection.initContract().methods.removeScheme(id).send({from : '0x320f78b8930dAe81880f2F63E7Cf1371a7700aFd'})
-            const allSchemes =  await connection.initContract().methods.allSchemes().call();
-            res.status(200).json({allSchemes: allSchemes})
+            const result = await connection.initContract().methods.removeScheme(scheme_id).send({from : '0xc83DF5D66DBb599072D80bCd9712c6D717eC8528'})
+            res.status(200).json({success: "Deleted"})
             // return  res.render("govDashboard",{isValid:true,userRole:req.session.userRole})
 
         } catch (error) {
@@ -59,5 +66,14 @@ module.exports = {
             res.status(404).json({error: error})
         }
 
+    },
+    updateScheme: async (req, res) => {
+        const scheme_id = parseInt(req.params.id)
+        const scheme = await connection.initContract().methods.getScheme(scheme_id).call()
+        .catch(error => {
+            console.log(error.message);
+            res.send("error")
+        })
+        res.render('updateScheme', {scheme: "sai"})
     }
 };
